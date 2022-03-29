1f04823a1dd494ba570ec06dd2194963048d4bb3
Maryln Peanuts <mpeanuts@ox-t5.msk.21-school.ru>
2022-03-29 19:44:49 +0300

    bug fix

--- source/errors.c
+++ source/errors.c
@@ -6,7 +6,7 @@
 /*   By: mpeanuts <mpeanuts@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 20:22:33 by mpeanuts          #+#    #+#             */
-/*   Updated: 2022/03/29 18:37:04 by mpeanuts         ###   ########.fr       */
+/*   Updated: 2022/03/29 19:42:08 by mpeanuts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ void	fail_exit(t_two_stacks *stacks)
 	ft_putendl_fd("Error", 2);
 	clear_stack(stacks->a);
 	clear_stack(stacks->b);
-	free(stacks->sorted_array);
+	if (stacks->sorted_array)
+		free(stacks->sorted_array);
 	free(stacks);
 	exit(EXIT_FAILURE);
 }
@@ -26,7 +27,8 @@ void	success_exit(t_two_stacks *stacks)
 {
 	clear_stack(stacks->a);
 	clear_stack(stacks->b);
-	free(stacks->sorted_array);
+	if (stacks->sorted_array)
+		free(stacks->sorted_array);
 	free(stacks);
 	exit(EXIT_SUCCESS);
 }
--- source/main.c
+++ source/main.c
@@ -6,59 +6,18 @@
 /*   By: mpeanuts <mpeanuts@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 15:37:36 by mpeanuts          #+#    #+#             */
-/*   Updated: 2022/03/29 18:48:35 by mpeanuts         ###   ########.fr       */
+/*   Updated: 2022/03/29 19:43:14 by mpeanuts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
-
-// void	print_all_stacks(t_stack *a, t_stack *b)
-// {
-// 	t_stack_elem	*tmp_a = a->top;
-// 	t_stack_elem	*tmp_b = b->top;
-
-// 	// printf("debug\n");
-// 	while (tmp_a)
-// 	{
-// 		// printf("debug_a\n");
-// 		printf("stack[%d]=%d\n", tmp_a->order, tmp_a->data);
-// 		tmp_a = tmp_a->next;
-// 	}
-// 	printf("----------------\n");
-// 	while (tmp_b)
-// 	{
-// 		// printf("debug_b\n");
-// 		printf("stack[%d]=%d\n", tmp_b->order, tmp_b->data);
-// 		tmp_b = tmp_b->next;
-// 	}
-// 	printf("\n\n\n");
-
-// }
-
-// void	print_with_size(t_two_stacks *stacks)
-// {
-// 	t_stack_elem	*tmp_a = stacks->a->top;
-// 	t_stack_elem	*tmp_b = stacks->b->top;
-// 	printf("size of stack a: %d\n", stacks->a->stack_size);
-// 	while (tmp_a)
-// 	{
-// 		printf("stack[%d]=%d\n", tmp_a->order, tmp_a->data);
-// 		tmp_a = tmp_a->next;	
-// 	}
-// 	printf("size of stack b: %d\n", stacks->b->stack_size);
-// 	while (tmp_b)
-// 	{
-// 		printf("stack[%d]=%d\n", tmp_b->order, tmp_b->data);
-// 		tmp_b = tmp_b->next;
-// 	}
-// 	printf("\n\n\n");
-// }
 
 int	main(int argc, char **argv)
 {
 	t_two_stacks	*stacks;
 
 	stacks = (t_two_stacks *)malloc(sizeof(t_two_stacks));
+	stacks->sorted_array = NULL;
 	if (argc == 1)
 		return (0);
 	stacks->a = create_stack();
